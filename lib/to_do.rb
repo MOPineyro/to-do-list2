247f15bfb509a0f6c78ec11f51afb645c9ab9546
Spyder Monkee McHenry & Manuel Omar Pineyro <student@epicodus.com>
2014-03-11 12:31:44 -0700

    multiple lists, sorts by priority, due date, or description.  allows user to remove marked tasks from lists

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -1,4 +1,22 @@
+require 'date'
+
 class Task
+  @@all_tasks = []
+
+  def Task.create(description)
+    task = Task.new(description)
+    @@all_tasks << task
+    task
+  end
+
+  def Task.all
+    @@all_tasks
+  end
+
+  def Task.clear
+    @@all_tasks =[]
+  end
+
   def initialize(description)
     @description = description
   end
@@ -10,9 +28,46 @@ class Task
   def marked_done
     @marked_done = true
   end
+
+  def set_due_date(date)
+    @due_date = Date.parse(date)
+  end
+
+  def due_date
+    @due_date
+  end
+
+  def set_priority(number)
+    if number.to_i > 10 || number.to_i < 0
+      puts 'Please enter a priority from 1-10 only'
+    else
+      @priority = number.to_i
+    end
+  end
+
+  def priority
+    @priority
+  end
+
 end
 
 class List
+  @@list_of_lists = []
+
+  def List.all
+    @@list_of_lists
+  end
+
+  def List.clear
+    @@list_of_lists = []
+  end
+
+  def List.create(list_name)
+    list = List.new(list_name)
+    @@list_of_lists << list_name
+    list
+  end
+
   def initialize(list_name)
     @list_name = list_name
     @tasks = []
@@ -22,7 +77,41 @@ class List
     @tasks
   end
 
-  def add_task(task_to_add)
-    @tasks << task_to_add
+  def list_name
+    @list_name
+  end
+
+  def add_task(description)
+    @tasks << Task.create(description)
+  end
+
+  def list_tasks
+    @tasks.each_with_index do |task, index|
+      puts "#{index + 1}. #{task.description}\n"
+    end
+  end
+
+  def remove_task(number)
+    @tasks.delete(@tasks[number.to_i-1])
+  end
+
+  def sort(sort_type)
+    if sort_type == "p"
+      @tasks.sort_by!{|task| task.priority}
+    elsif sort_type == "d"
+      @tasks.sort_by!{|task| task.description}
+    elsif sort_type == 'dd'
+      @tasks.sort_by!{|task| task.due_date}
+    else
+      puts 'Please choose one of the following: p- priority, d- description, dd due_date'
+    end
   end
 end
+
+
+
+
+
+
+
+
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,12 +1,29 @@
 require 'rspec'
 require 'to_do'
 
-
 describe 'Task' do
+
+  before do
+    Task.clear
+  end
+
   it 'is initialized with a description' do
     test_task = Task.new 'this is a task description'
     test_task.should be_an_instance_of Task
   end
+
+  describe '.create' do
+    it 'makes a new instance of Task' do
+      test_task = Task.create('wash the lion')
+      test_task.should be_an_instance_of Task
+    end
+
+    it 'saves the Task' do
+      test_task = Task.create('wash the lion')
+      Task.all.should eq [test_task]
+    end
+  end
+
 
   describe '#description' do
     it 'lets you read the description out' do
@@ -21,20 +38,75 @@ describe 'Task' do
       test_task.marked_done.should eq true
     end
   end
+
+  describe '#set_due_date' do
+    it 'takes a date from user and applies it to the task' do
+    test_task = Task.new('breathe fire')
+    test_task.set_due_date('january 4th, 2050')
+    test_task.due_date.should eq Date.parse('january 4th, 2050')
+    end
+  end
+
+  describe '#set_priority' do
+    it 'gives the task a priority setting from 1-10' do
+      test_task = Task.new('dip the cat in the tub')
+      test_task.set_priority('4')
+      test_task.priority.should eq 4
+    end
+  end
+
 end
 
 describe 'List' do
+  before do
+    List.clear
+  end
+
   it 'is initialized with a place to add tasks' do
     test_list = List.new('grocery list')
     test_list.should be_an_instance_of List
   end
 
+  describe '.create' do
+    it 'creates a new list' do
+      test_list = List.create('chores')
+      test_list.should be_an_instance_of List
+    end
+
+    it 'puts it into the list of lists' do
+      test_list = List.create('chores')
+      List.all.length.should eq 1
+    end
+  end
+
+
+
   describe '#add_task' do
     it 'adds a task to the current list' do
       test_list = List.new('games')
-      task_to_add = Task.new('monkee bizness')
-      test_list.add_task(task_to_add)
-      test_list.tasks.should eq [task_to_add]
+      task_to_add = test_list.add_task('monkee bizness')
+      test_list.tasks.should eq task_to_add
+    end
+  end
+
+  describe '#remove_task' do
+    it 'removes the task from the current list' do
+      test_list = List.new('games')
+      task_to_add = test_list.add_task('monkee bizness')
+      test_list.remove_task('1')
+      test_list.tasks.should eq []
+    end
+  end
+
+  describe '#sort' do
+    it 'sorts the list of tasks by due date, priority, or description' do
+      test_list = List.new('games')
+      task_to_add = test_list.add_task('monkee bizness')
+      test_list.tasks[0].set_priority('5')
+      task_add = test_list.add_task('manuel bizness')
+      test_list.tasks[1].set_priority('4')
+      test_list.sort('p')
+      test_list.tasks[0].description.should eq 'manuel bizness'
     end
   end
 end
